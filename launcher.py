e146dfaaec9ee243229e3eb72f9c3a10235cdde0
xdanielsb <dniel096@gmail.com>
2016-05-30 18:16:34 -0500

    unneccesary

--- launcher.py
+++ launcher.py
@@ -3,8 +3,7 @@ from flask import Flask, render_template,request, flash,  url_for, redirect, ses
 app=Flask(__name__)
 app.secret_key = '15%&*^&^GJHYTDT24623/*@!@#G@JH$%+9'
 
-if __name__ == "__main__":
-    app.run(debug= True)
+
 
 @app.route("/")
 def index():
@@ -29,3 +28,7 @@ def twoPlayers():
     except Exception as e:
         return "Error in the method two G"
 
+
+#Put this at the end for the app recognize the before methods
+if __name__ == "__main__":
+    app.run(debug= True)
